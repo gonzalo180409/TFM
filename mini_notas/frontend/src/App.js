e5fed88fe428b4177326412fd7426951674a9a74
gonzalo180409 <gonzalo.solor00@gmail.com>
2025-04-24 12:49:51 +0200

    calendario front mock

--- mini_notas/frontend/src/App.js
+++ mini_notas/frontend/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
+import MyCalendar from './Calendar';
+import './App.css';
 
 function App({ signOut, user }) {
   return (
-    <div className="App">
-      <h1>Bienvenido {user.username}</h1>
-      <button onClick={signOut}>Cerrar sesión</button>
+    <div className="app-container">
+      <h1 className="welcome-title">Bienvenido {user.username}</h1>
+      <button className="signout-btn" onClick={signOut}>Cerrar sesión</button>
+      <MyCalendar />
     </div>
   );
 }
